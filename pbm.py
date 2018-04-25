36d2e9e3f48d102b07cab0408d9d77fb384b8bb1
fiorinin <contact@graphyx.fr>
2018-04-25 11:53:03 -0400

    Fixed locale

--- pbm.py
+++ pbm.py
@@ -10,8 +10,10 @@ Usage:
     pbm export
 """
 from docopt import docopt
-import _locale
-_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
+import locale
+def getpreferredencoding(do_setlocale = True):
+    return "utf-8"
+locale.getpreferredencoding = getpreferredencoding
 
 # TODO: add controls to verify the workflow (e.g. not to train before loading articles)
 def main():
