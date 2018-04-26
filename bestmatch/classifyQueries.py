ea31266c8932aaea918670961e7392a4a361c416
fiorinin <contact@graphyx.fr>
2018-04-26 16:11:16 -0400

    More informative query classification

--- bestmatch/classifyQueries.py
+++ bestmatch/classifyQueries.py
@@ -20,6 +20,12 @@ def query_type(q):
     return 'SIMPLE'
 
 # Query classification
+print("Classifying queries...")
+regex_nb = 0
+field_nb = 0
+phrase_nb = 0
+boolean_nb = 0
+simple_nb = 0
 with open(base_path+"queries.txt", encoding='utf8') as f, \
      open(sorted_path+"regex_queries.txt", "w", encoding='utf8') as regex_file, \
      open(sorted_path+"field_queries.txt", "w", encoding='utf8') as field_file, \
@@ -31,11 +37,22 @@ with open(base_path+"queries.txt", encoding='utf8') as f, \
         qtype = query_type(val)
         if qtype == "REGEX":
             regex_file.write(key+"|"+val.strip()+"\n")
+            regex_nb += 1
         elif qtype == "FIELD":
             field_file.write(key+"|"+val.strip()+"\n")
+            field_nb += 1
         elif qtype == "PHRASE":
             phrase_file.write(key+"|"+val.strip()+"\n")
+            phrase_nb += 1
         elif qtype == "BOOLEAN":
             boolean_file.write(key+"|"+val.strip()+"\n")
+            boolean_nb += 1
         else:
             simple_file.write(key+"|"+val.strip()+"\n")
+            simple_nb += 1
+
+print(str(regex_nb)+" queries contained regular expressions.")
+print(str(field_nb)+" queries contained fields (e.g. [author]).")
+print(str(phrase_nb)+" queries contained quoted phrases.")
+print(str(boolean_nb)+" queries contained Boolean expressions.")
+print(str(simple_nb)+" queries were classified as 'simple'. These will be used for training the model.")
